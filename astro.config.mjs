3d7432218cafbf1b35aaae97ec1598c7a7917e3e
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2024-03-19 10:36:43 +0000

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,28 @@
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
+export default defineConfig({
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      components: {
+        page: "storyblok/Page",
+        config: "storyblok/Config",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+      },
+    }),
+    tailwind(),
+  ],
+  vite: {
+    plugins: [basicSsl()],
+    server: {
+      https: true,
+    },
+  },
+});
