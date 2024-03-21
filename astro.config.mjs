c225af12e593fc67afe3b2aa706fc25420ea9093
K-velvet-P <123256886+K-velvet-P@users.noreply.github.com>
2024-03-21 11:23:57 +0000

    deployment error fix

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,36 +1,39 @@
-import { defineConfig } from 'astro/config';
-import storyblok from '@storyblok/astro';
-import { loadEnv } from 'vite';
-import tailwind from '@astrojs/tailwind';
-import basicSsl from '@vitejs/plugin-basic-ssl';
+import { defineConfig } from "astro/config";
+import storyblok from "@storyblok/astro";
+import { loadEnv } from "vite";
+import tailwind from "@astrojs/tailwind";
+import basicSsl from "@vitejs/plugin-basic-ssl";
 import vercel from "@astrojs/vercel/serverless";
-const env = loadEnv('', process.cwd(), 'STORYBLOK');
- 
+const env = loadEnv("", process.cwd(), "STORYBLOK");
+
 // https://astro.build/config
 export default defineConfig({
-  integrations: [storyblok({
-    accessToken: env.STORYBLOK_TOKEN,
-    bridge: env.STORYBLOK_IS_PREVIEW === 'yes',
-    components: {
-      page: 'storyblok/Page',
-      feature: 'storyblok/Feature',
-      grid: 'storyblok/Grid',
-      teaser: 'storyblok/Teaser',
-      hero: 'storyblok/Hero',
-      config: 'storyblok/Config',
-      'popular-articles': 'storyblok/PopularArticles',
-      'all-articles': 'storyblok/AllArticles',
-      article: 'storyblok/Article'
-    }
-  }), tailwind()],
-  output: env.STORYBLOK_IS_PREVIEW === 'yes' ? 'server' : 'static',
-  ...(env.STORYBLOK_ENV === 'development' && {
+  integrations: [
+    storyblok({
+      accessToken: env.STORYBLOK_TOKEN,
+      bridge: env.STORYBLOK_IS_PREVIEW === "yes",
+      components: {
+        page: "storyblok/Page",
+        feature: "storyblok/Feature",
+        grid: "storyblok/Grid",
+        teaser: "storyblok/Teaser",
+        hero: "storyblok/Hero",
+        config: "storyblok/Config",
+        "popular-articles": "storyblok/PopularArticles",
+        "all-articles": "storyblok/AllArticles",
+        article: "storyblok/Article",
+      },
+    }),
+    tailwind(),
+  ],
+  output: env.STORYBLOK_IS_PREVIEW === "yes" ? "server" : "static",
+  ...(env.STORYBLOK_ENV === "development" && {
     vite: {
       plugins: [basicSsl()],
       server: {
-        https: true
-      }
-    }
+        https: true,
+      },
+    },
   }),
-  adapter: vercel()
+  adapter: vercel(),
 });
